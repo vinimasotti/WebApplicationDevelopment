9ab8721a59b5ae12cc3cb34159efad74140bda28
vinimasotti <vini_masotti@hotmail.com>
2022-10-27 12:01:12 +0100

    v1

---
+++ Javascript/lesson1.js
@@ -0,0 +1,68 @@
+<DOCTYPE html>
+  <html>
+  <head>
+          <title>Javascript</title>
+  </head>
+  
+  <body>
+    
+    <script> 
+      
+      
+      var number = prompt (" insert a number");
+      var number2 = prompt (" insert another number");
+      
+      if (number >=40){
+        document.write ("\npass");
+      }else {
+        document.write ("\nfail");
+      }
+      
+      if (number <40){
+        document.write ("\nfail.");
+      }else {
+        document.write ("\npass.30  ");
+      }
+      
+      
+
+      for (var i = 0; i < 1; i++) {
+        
+
+          alert("1");
+
+      }
+      for (var i = 0; i < 1; i++) {
+        
+
+        alert("2");
+
+    }
+    for (var i = 0; i < 1; i++) {
+        
+
+        alert("3");
+
+    }
+    for (var i = 0; i < 1; i++) {
+        
+
+        alert("4");
+
+    }
+
+    for (var i = 0; i < 1; i++) {
+        
+
+        alert("5");
+
+    }
+  </script>
+  <br>
+  <script>
+    document.write("hello")
+    </script>
+
+        
+        
+   </body>
